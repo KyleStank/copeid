d4257aea8ab3d15384c19618fb0f820725ca89a7
Kyle Stankovich <gamemakergml@gmail.com>
2021-06-23 18:41:35 -0400

    Added @angular/material

--- projects/app/src/app/app-routing.module.ts
+++ projects/app/src/app/app-routing.module.ts
@@ -9,7 +9,11 @@ const routes: Routes = [
   },
   {
     path: 'dashboard',
-    loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardPageModule)
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardPageModule),
+    data: {
+      showHeader: false,
+      showFooter: false
+    }
   }
 ];
 
--- projects/app/src/app/pages/dashboard/dashboard.module.ts
+++ projects/app/src/app/pages/dashboard/dashboard.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardPageComponent } from './dashboard.component';
@@ -7,6 +8,7 @@ import { DashboardPageComponent } from './dashboard.component';
 @NgModule({
   imports: [
     CommonModule,
+    MatButtonModule,
     DashboardRoutingModule
   ],
   declarations: [DashboardPageComponent]
