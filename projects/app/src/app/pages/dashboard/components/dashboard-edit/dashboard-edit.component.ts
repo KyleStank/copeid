50421e36b5c6682aa30089f01bf3d6704351bea8
Kyle Stankovich <gamemakergml@gmail.com>
2021-07-05 14:51:50 -0400

    Added new services to DashboardEditPageComponent, for testing purposes only

--- projects/app/src/app/pages/dashboard/components/dashboard-edit/dashboard-edit.component.ts
+++ projects/app/src/app/pages/dashboard/components/dashboard-edit/dashboard-edit.component.ts
@@ -2,14 +2,21 @@ import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
-import { Genus, GenusService } from '@app/features';
+import {
+  Genus,
+  GenusService,
+  Photograph,
+  PhotographService,
+  Specimen,
+  SpecimenService
+} from '@app/features';
 
 @Component({
   selector: 'app-dashboard-edit',
   templateUrl: './dashboard-edit.component.html',
   styleUrls: ['./dashboard-edit.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [GenusService]
+  providers: [GenusService, PhotographService, SpecimenService]
 })
 export class DashboardEditPageComponent implements OnInit, OnDestroy {
   private readonly _destroyed = new Subject<void>();
@@ -17,13 +24,31 @@ export class DashboardEditPageComponent implements OnInit, OnDestroy {
   private readonly _genuses = new BehaviorSubject<Genus[]>([]);
   readonly genuses$: Observable<Genus[]>;
 
-  constructor(private readonly _genusService: GenusService) {
+  private readonly _photographs = new BehaviorSubject<Photograph[]>([]);
+  readonly photographs$: Observable<Photograph[]>;
+
+  private readonly _specimens = new BehaviorSubject<Specimen[]>([]);
+  readonly specimens$: Observable<Specimen[]>;
+
+  constructor(
+    private readonly _genusService: GenusService,
+    private readonly _photographService: PhotographService,
+    private readonly _specimenService: SpecimenService
+  ) {
     this.genuses$ = this._genuses.asObservable()
+      .pipe(takeUntil(this._destroyed));
+
+    this.photographs$ = this._photographs.asObservable()
+      .pipe(takeUntil(this._destroyed));
+
+    this.specimens$ = this._specimens.asObservable()
       .pipe(takeUntil(this._destroyed));
   }
 
   ngOnInit(): void {
     this._genusService.getAllEntities().subscribe(this._genuses.next.bind(this._genuses));
+    this._photographService.getAllEntities().subscribe(this._photographs.next.bind(this._photographs));
+    this._specimenService.getAllEntities().subscribe(this._specimens.next.bind(this._specimens));
   }
 
   ngOnDestroy(): void {
