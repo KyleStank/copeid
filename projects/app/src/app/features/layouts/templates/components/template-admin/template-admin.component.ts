4eb47222f64cef68c4903f0d8f8581b9efdc44f0
Kyle Stankovich <gamemakergml@gmail.com>
2021-07-13 22:13:52 -0400

    Added keyboard shortcut for toggling nav

--- projects/app/src/app/features/layouts/templates/components/template-admin/template-admin.component.ts
+++ projects/app/src/app/features/layouts/templates/components/template-admin/template-admin.component.ts
@@ -1,13 +1,24 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { ChildActivationStart } from '@angular/router';
+import { KeyboardService, KeyCode } from '@stankng/services';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-template-admin',
   templateUrl: './template-admin.component.html',
   styleUrls: ['./template-admin.component.scss'],
+  providers: [KeyboardService],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TemplateAdminComponent implements OnDestroy {
+  private readonly _destroyed = new Subject<void>();
+
+  @ViewChild('sidenav', { static: true, read: MatSidenav })
+  sidenav: MatSidenav | undefined;
+
   @Input()
   appName: string = 'Admin';
 
@@ -18,28 +29,45 @@ export class TemplateAdminComponent implements OnDestroy {
   menu: { label: string; url?: string; logo?: string; }[] = [];
 
   mobileQuery: MediaQueryList;
-  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
-  fillerContent = Array.from({length: 50}, () =>
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
-
   private _mobileQueryListener: () => void;
 
   constructor(
-    private readonly _changeDetectorRef: ChangeDetectorRef,
+    private readonly _detectorRef: ChangeDetectorRef,
+    private readonly _keyboardService: KeyboardService,
     private readonly _media: MediaMatcher
   ) {
+    // Setup mobile query listener.
     this.mobileQuery = this._media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => this._changeDetectorRef.detectChanges();
+    this._mobileQueryListener = () => this._detectorRef.markForCheck();
     this.mobileQuery.addEventListener('change', this._mobileQueryListener);
+
+    this._registerKeyboardShorts();
+  }
+
+  private _registerKeyboardShorts(): void {
+    // Commands:
+    //  - Toggle_Sidenav
+    const toggleSidenavFn = () => {
+      this.sidenav?.toggle();
+      this._detectorRef.markForCheck();
+    };
+
+    // Toggle_Sidenav
+    this._keyboardService.shortcut$([KeyCode.LeftControl, KeyCode.B])
+      .pipe(takeUntil(this._destroyed))
+      .subscribe(toggleSidenavFn.bind(this));
+
+    this._keyboardService.shortcut$([KeyCode.RightControl, KeyCode.B])
+      .pipe(takeUntil(this._destroyed))
+      .subscribe(toggleSidenavFn.bind(this));
   }
 
   ngOnDestroy(): void {
     if (this.mobileQuery.removeAllListeners) {
       this.mobileQuery.removeAllListeners();
     }
+
+    this._destroyed.next();
+    this._destroyed.complete();
   }
 }
