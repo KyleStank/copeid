a06becd74a75206be4b1a1b2384260445ae6bea3
Kyle Stankovich <gamemakergml@gmail.com>
2021-07-13 20:37:41 -0400

    Started creating contributors admin page

--- projects/app/src/app/pages/admin/admin.module.ts
+++ projects/app/src/app/pages/admin/admin.module.ts
@@ -2,7 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 
 import {
   AdminContributorsPageComponent,
@@ -20,6 +24,10 @@ import { AdminRoutingModule } from './admin-routing.module';
     MatButtonModule,
     MatCardModule,
     MatIconModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
     AdminRoutingModule
   ],
   declarations: [
--- projects/app/src/app/pages/admin/components/admin-contributors/admin-contributors.component.ts
+++ projects/app/src/app/pages/admin/components/admin-contributors/admin-contributors.component.ts
@@ -1,9 +1,60 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { Contributor, ContributorService } from '@app/features';
 
 @Component({
   selector: 'app-admin-contributors',
   templateUrl: './admin-contributors.component.html',
   styleUrls: ['./admin-contributors.component.scss'],
+  providers: [ContributorService],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AdminContributorsPageComponent {}
+export class AdminContributorsPageComponent implements OnInit, AfterViewInit, OnDestroy {
+  private readonly _destroyed = new Subject<void>();
+
+  private readonly _contributors = new BehaviorSubject<Contributor[]>([]);
+  readonly contributors$: Observable<Contributor[]>;
+
+  readonly dataSource = new MatTableDataSource<Contributor>();
+  readonly columns = ['name', 'options'];
+
+  @ViewChild(MatSort, { static: true })
+  sort: MatSort | undefined;
+
+  @ViewChild(MatPaginator, { static: true })
+  paginator: MatPaginator | undefined;
+
+  constructor(
+    private readonly _detectorRef: ChangeDetectorRef,
+    private readonly _contributorService: ContributorService
+  ) {
+    this.contributors$ = this._contributors.asObservable()
+      .pipe(takeUntil(this._destroyed));
+  }
+
+  ngOnInit(): void {
+    this._contributorService.getAllEntities().subscribe(this._contributors.next.bind(this._contributors));
+
+    this.contributors$.subscribe((contributors: Contributor[]) => this.dataSource.data = contributors);
+  }
+
+  ngAfterViewInit(): void {
+    if (this.sort) {
+      this.dataSource.sort = this.sort;
+    }
+
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this._destroyed.next();
+    this._destroyed.complete();
+  }
+}
