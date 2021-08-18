f8bed40ff4f29611ce36c7959332e8f0e8c473ea
Kyle Stankovich <gamemakergml@gmail.com>
2021-08-17 23:13:25 -0400

    Fixed bug where reference page animations would have no stagger

--- projects/app/src/app/pages/references/components/references/references.component.ts
+++ projects/app/src/app/pages/references/components/references/references.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { ChangeDetectionStrategy, Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, combineLatest, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 import { Contributor, ContributorService, Reference, ReferenceService } from '@app/features';
@@ -29,12 +29,19 @@ export class ReferencesPageComponent implements OnInit, OnDestroy {
   private readonly _referencesSubject = new BehaviorSubject<Reference[]>([]);
   readonly references$ = this._referencesSubject.asObservable();
 
+  @HostBinding('@pageAnimations')
+  animatePage: boolean = false;
+
   constructor(
     private readonly _contributorService: ContributorService,
     private readonly _referenceService: ReferenceService
   ) {
     this.contributors$ = this.contributors$.pipe(takeUntil(this._destroyed));
     this.references$ = this.references$.pipe(takeUntil(this._destroyed));
+
+    combineLatest([this.contributors$, this.references$])
+      .pipe(takeUntil(this._destroyed))
+      .subscribe(() => this.animatePage = !this.animatePage);
   }
 
   ngOnInit(): void {
