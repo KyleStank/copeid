5cd636b1f8f25e688194e318c5a8395ee0ad1e6a
Kyle Stankovich <gamemakergml@gmail.com>
2021-08-26 08:50:56 -0400

    Removed unused components within layouts

--- projects/app/src/app/features/layouts/headers/components/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './header-default/header-default.component';
--- projects/app/src/app/features/layouts/index.ts
+++ projects/app/src/app/features/layouts/index.ts
@@ -1,7 +1,4 @@
 export * from './directives';
-export * from './footers';
-export * from './headers';
 export * from './models';
-export * from './sections';
 export * from './services';
 export * from './templates';
