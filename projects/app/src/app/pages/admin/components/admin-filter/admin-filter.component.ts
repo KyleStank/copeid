1613823ffded0bed003d31918d25e18c3430fa81
Kyle Stankovich <gamemakergml@gmail.com>
2021-08-29 17:39:22 -0400

    Added FilterModelService and FilterService to AdminFilterPageComponent

--- projects/app/src/app/pages/admin/components/admin-filter/admin-filter.component.ts
+++ projects/app/src/app/pages/admin/components/admin-filter/admin-filter.component.ts
@@ -1,11 +1,42 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
+
+import { Filter, FilterModel, FilterModelService, FilterService } from '@app/features';
 
 @Component({
   selector: 'app-admin-filter',
   templateUrl: './admin-filter.component.html',
   styleUrls: ['./admin-filter.component.scss'],
+  providers: [
+    FilterModelService,
+    FilterService
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AdminFilterPageComponent {
+export class AdminFilterPageComponent implements OnInit, OnDestroy {
+  private readonly _destroyed = new Subject<void>();
 
+  private readonly _filterModelsSubject = new BehaviorSubject<FilterModel[]>([]);
+  readonly filterModels$ = this._filterModelsSubject.asObservable();
+
+  private readonly _filtersSubject = new BehaviorSubject<Filter[]>([]);
+  readonly filters$ = this._filtersSubject.asObservable();
+
+  constructor(
+    private readonly _filterModelService: FilterModelService,
+    private readonly _filterService: FilterService
+  ) {
+    this.filterModels$ = this.filterModels$.pipe(takeUntil(this._destroyed));
+    this.filters$ = this.filters$.pipe(takeUntil(this._destroyed));
+  }
+
+  ngOnInit(): void {
+    this._filterModelService.getAll().subscribe(this._filterModelsSubject.next.bind(this._filterModelsSubject));
+    this._filterService.getAll().subscribe(this._filtersSubject.next.bind(this._filtersSubject));
+  }
+
+  ngOnDestroy(): void {
+    this._destroyed.next();
+    this._destroyed.complete();
+  }
 }
