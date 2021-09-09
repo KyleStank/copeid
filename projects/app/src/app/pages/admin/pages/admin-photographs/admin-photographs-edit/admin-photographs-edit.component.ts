8443aa5fcd945b180371f08185a61af30d4bbf17
Kyle Stankovich <gamemakergml@gmail.com>
2021-09-09 14:01:36 -0400

    Added back admin photographs page functionality

--- projects/app/src/app/pages/admin/pages/admin-photographs/admin-photographs-edit/admin-photographs-edit.component.ts
+++ projects/app/src/app/pages/admin/pages/admin-photographs/admin-photographs-edit/admin-photographs-edit.component.ts
@@ -1,6 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject, Observable, Subject, takeUntil } from 'rxjs';
 
-import { PhotographService } from '@app/features';
+import { Photograph, PhotographService } from '@app/features';
 
 @Component({
   selector: 'app-admin-photographs-edit',
@@ -12,5 +15,62 @@ import { PhotographService } from '@app/features';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AdminPhotographsEditComponent {
+  readonly destroyed = new Subject<void>();
 
+  private readonly _modelSubject = new BehaviorSubject<Photograph | undefined>(undefined);
+  readonly model$ = this._modelSubject.asObservable();
+
+  readonly formGroup = this._fb.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    alt: [''],
+    url: ['', Validators.required]
+  });
+
+  id: string | undefined;
+
+  constructor(
+    private readonly _activatedRoute: ActivatedRoute,
+    private readonly _changeDetectorRef: ChangeDetectorRef,
+    private readonly _photographService: PhotographService,
+    private readonly _fb: FormBuilder
+  ) {
+    this.model$ = this.model$.pipe(takeUntil(this.destroyed));
+    this.model$.subscribe({
+      next: result => {
+        if (!!result) {
+          this.formGroup.patchValue({
+            title: result.title,
+            description: result.description,
+            alt: result.alt,
+            url: result.url
+          });
+        }
+
+        this.formGroup.markAllAsTouched();
+        this._changeDetectorRef.markForCheck();
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.id = this._activatedRoute.snapshot.paramMap.get('id') ?? undefined;
+    if (!!this.id) {
+      this._photographService.getSingle(this.id).subscribe(this._modelSubject.next.bind(this._modelSubject));
+    }
+  }
+
+  save(): Observable<Photograph> {
+    const model: Photograph = {
+      ...this.formGroup.value,
+      id: this.id
+    };
+
+    return !!model.id ? this._photographService.update(model) : this._photographService.create(model);
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
 }
--- projects/app/src/app/pages/admin/pages/admin-photographs/admin-photographs-manage/admin-photographs-manage.component.ts
+++ projects/app/src/app/pages/admin/pages/admin-photographs/admin-photographs-manage/admin-photographs-manage.component.ts
@@ -1,6 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BehaviorSubject, skipWhile, Subject, takeUntil } from 'rxjs';
 
-import { PhotographService } from '@app/features';
+import { Photograph, PhotographService } from '@app/features';
+import { ConfirmationAlertModalCompoonent } from '@shared/modals/confirmation-alert';
+import { AdminColumn } from '../../../common';
+import { IAdminManageView } from '../../../components';
 
 @Component({
   selector: 'app-admin-photographs-manage',
@@ -11,6 +17,79 @@ import { PhotographService } from '@app/features';
   providers: [PhotographService],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AdminPhotographsManageComponent {
+export class AdminPhotographsManageComponent implements IAdminManageView, OnInit, OnDestroy {
+  readonly destroyed = new Subject<void>();
 
+  private readonly _photographsSubject = new BehaviorSubject<Photograph[]>([]);
+  readonly photographs$ = this._photographsSubject.asObservable();
+  public readonly columns: AdminColumn[] = [
+    { title: 'Title', property: 'title' },
+    { title: 'Description', property: 'description' },
+    { title: 'Alt. Title', property: 'alt' },
+    { title: 'URL', property: 'url' }
+  ];
+  selectedItems: any[] = [];
+
+  constructor(
+    private readonly _activatedRoute: ActivatedRoute,
+    private readonly _photographService: PhotographService,
+    private readonly _dialog: MatDialog,
+    private readonly _router: Router
+  ) {
+    this.photographs$ = this.photographs$.pipe(takeUntil(this.destroyed));
+  }
+
+  ngOnInit(): void {
+    this.getEntities();
+  }
+
+  getEntities(): void {
+    this._photographService.getAll({
+      orderBy: ['title']
+    }).subscribe(this._photographsSubject.next.bind(this._photographsSubject));
+  }
+
+  editAddItem(model?: Photograph): void {
+    if (!!model?.id) {
+      this._router.navigate(['edit', model.id], { relativeTo: this._activatedRoute });
+    } else {
+      this._router.navigate(['create'], { relativeTo: this._activatedRoute });
+    }
+  }
+
+  deleteItems(models?: Photograph[]): void {
+    models = models ?? [];
+    if (models.length === 0) return;
+
+    const isSingle = models.length === 1;
+    const modelName = isSingle ? 'Photograph' : 'Photographs';
+    const dialogRef = this._dialog.open(ConfirmationAlertModalCompoonent, {
+      data: {
+        title: `Delete ${modelName}?`,
+        message: `Are you sure you want to delete ${isSingle ? 'this' : 'these'} ${modelName}?`
+      }
+    });
+
+    dialogRef.afterClosed()
+      .pipe(
+        takeUntil(this.destroyed),
+        skipWhile((result: boolean) => !result)
+      ).subscribe({
+        next: () => {
+          models!.forEach(m => {
+            if (!!m?.id) {
+              this._photographService.delete(m.id).subscribe({
+                next: () => this.getEntities(),
+                error: (error: any) => console.error('Error:', error)
+              });
+            }
+          });
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
 }
