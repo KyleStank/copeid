13998c3a039ab7607a1bdbc18c9b2fdb1f162282
Kyle Stankovich <gamemakergml@gmail.com>
2021-09-12 19:25:05 -0400

    Added functionality to filter admin page

--- projects/app/src/app/pages/admin/pages/admin-filters/admin-filters-edit/admin-filters-edit.component.ts
+++ projects/app/src/app/pages/admin/pages/admin-filters/admin-filters-edit/admin-filters-edit.component.ts
@@ -1,5 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FilterService } from '@app/features';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject, Observable, Subject, takeUntil } from 'rxjs';
+
+import { Filter, FilterModel, FilterModelService, FilterService } from '@app/features';
+import { IAdminEditView } from '../../../components';
 
 @Component({
   selector: 'app-admin-filters-edit',
@@ -7,9 +12,69 @@ import { FilterService } from '@app/features';
   host: {
     'class': 'd-block'
   },
-  providers: [FilterService],
+  providers: [FilterService, FilterModelService],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AdminFiltersEditComponent {
+export class AdminFiltersEditComponent implements IAdminEditView, OnInit, OnDestroy {
+  readonly destroyed = new Subject<void>();
 
+  private readonly _modelSubject = new BehaviorSubject<Filter | undefined>(undefined);
+  readonly model$ = this._modelSubject.asObservable();
+
+  private readonly _filterModelsSubject = new BehaviorSubject<FilterModel[]>([]);
+  readonly filterModels$ = this._filterModelsSubject.asObservable();
+
+  readonly formGroup = this._fb.group({
+    filterModelId: [null, Validators.required],
+    displayName: ['', Validators.required]
+  });
+
+  id: string | undefined;
+
+  constructor(
+    private readonly _activatedRoute: ActivatedRoute,
+    private readonly _changeDetectorRef: ChangeDetectorRef,
+    private readonly _filterService: FilterService,
+    private readonly _filterModelService: FilterModelService,
+    private readonly _fb: FormBuilder
+  ) {
+    this.model$ = this.model$.pipe(takeUntil(this.destroyed));
+    this.model$.subscribe({
+      next: result => {
+        if (!!result) {
+          this.formGroup.patchValue({
+            filterModelId: result.filterModelId,
+            displayName: result.displayName
+          });
+        }
+
+        this.formGroup.markAllAsTouched();
+        this._changeDetectorRef.markForCheck();
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.id = this._activatedRoute.snapshot.paramMap.get('id') ?? undefined;
+    if (!!this.id) {
+      this._filterService.getSingle(this.id).subscribe(this._modelSubject.next.bind(this._modelSubject));
+    }
+
+    this._filterModelService.getAll().subscribe(this._filterModelsSubject.next.bind(this._filterModelsSubject));
+  }
+
+  save(): Observable<Filter> {
+    const model: Filter = {
+      ...(this._modelSubject.value ?? {}),
+      ...this.formGroup.value,
+      id: this.id
+    };
+
+    return !!model.id ? this._filterService.update(model) : this._filterService.create(model);
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
 }
--- projects/app/src/app/pages/admin/pages/admin-filters/admin-filters-manage/admin-filters-manage.component.ts
+++ projects/app/src/app/pages/admin/pages/admin-filters/admin-filters-manage/admin-filters-manage.component.ts
@@ -1,5 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FilterService } from '@app/features';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BehaviorSubject, skipWhile, Subject, takeUntil } from 'rxjs';
+
+import { Filter, FilterService } from '@app/features';
+import { ConfirmationAlertModalCompoonent } from '@shared/modals/confirmation-alert';
+import { AdminColumn } from '../../../common';
+import { IAdminManageView } from '../../../components';
 
 @Component({
   selector: 'app-admin-filters-manage',
@@ -10,6 +17,74 @@ import { FilterService } from '@app/features';
   providers: [FilterService],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AdminFiltersManageComponent {
+export class AdminFiltersManageComponent implements IAdminManageView, OnInit, OnDestroy {
+  readonly destroyed = new Subject<void>();
 
+  private readonly _filtersSubject = new BehaviorSubject<Filter[]>([]);
+  readonly filters$ = this._filtersSubject.asObservable();
+  readonly columns: AdminColumn[] = [
+    { title: 'Display Name', property: 'displayName' }
+  ];
+  selectedItems: any[] = [];
+
+  constructor(
+    private readonly _activatedRoute: ActivatedRoute,
+    private readonly _filterService: FilterService,
+    private readonly _dialog: MatDialog,
+    private readonly _router: Router
+  ) {
+    this.filters$ = this.filters$.pipe(takeUntil(this.destroyed));
+  }
+
+  ngOnInit(): void {
+    this.getEntities();
+  }
+
+  getEntities(): void {
+    this._filterService.getAll().subscribe(this._filtersSubject.next.bind(this._filtersSubject));
+  }
+
+  editAddItem(model?: Filter): void {
+    if (!!model?.id) {
+      this._router.navigate(['edit', model.id], { relativeTo: this._activatedRoute });
+    } else {
+      this._router.navigate(['create'], { relativeTo: this._activatedRoute });
+    }
+  }
+
+  deleteItems(models?: Filter[]): void {
+    models = models ?? [];
+    if (models.length === 0) return;
+
+    const isSingle = models.length === 1;
+    const modelName = isSingle ? 'Filter' : 'Filters';
+    const dialogRef = this._dialog.open(ConfirmationAlertModalCompoonent, {
+      data: {
+        title: `Delete ${modelName}?`,
+        message: `Are you sure you want to delete ${isSingle ? 'this' : 'these'} ${modelName}?`
+      }
+    });
+
+    dialogRef.afterClosed()
+      .pipe(
+        takeUntil(this.destroyed),
+        skipWhile((result: boolean) => !result)
+      ).subscribe({
+        next: () => {
+          models!.forEach(m => {
+            if (!!m?.id) {
+              this._filterService.delete(m.id).subscribe({
+                next: () => this.getEntities(),
+                error: (error: any) => console.error('Error:', error)
+              });
+            }
+          });
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
 }
