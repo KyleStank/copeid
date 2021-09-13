0a1478343bb101b10cb3bf4aa5b6461baced5d5c
Kyle Stankovich <gamemakergml@gmail.com>
2021-09-13 02:35:06 -0400

    Removed unused filter service methods

--- projects/app/src/app/features/filter/services/filter-section.service.ts
+++ projects/app/src/app/features/filter/services/filter-section.service.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 
 import { environment } from '@app/environments';
 import { AbstractQueryableEntityService } from '@core/services/entity';
-import { FilterSection, FilterSectionOption, FilterSectionQuery } from '../models';
+import { FilterSection, FilterSectionQuery } from '../models';
 
 @Injectable()
 export class FilterSectionService extends AbstractQueryableEntityService<FilterSection, FilterSectionQuery> {
   public getEndpoint(): string {
     return `${environment.apiUrl}/FilterSection`;
-  }
-
-  public getOptions(id: string): Observable<FilterSectionOption[]> {
-    const endpoint = `${this._endpoint}/${id}/Options`;
-    return this._http.get<FilterSectionOption[]>(endpoint);
   }
 }
--- projects/app/src/app/features/filter/services/filter.service.ts
+++ projects/app/src/app/features/filter/services/filter.service.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 
 import { environment } from '@app/environments';
 import { AbstractQueryableEntityService } from '@core/services/entity';
-import { Filter, FilterQuery, FilterSection } from '../models';
+import { Filter, FilterQuery } from '../models';
 
 @Injectable()
 export class FilterService extends AbstractQueryableEntityService<Filter, FilterQuery> {
   public getEndpoint(): string {
     return `${environment.apiUrl}/Filter`;
-  }
-
-  public getSections(id: string): Observable<FilterSection[]> {
-    const endpoint = `${this._endpoint}/${id}/Sections`;
-    return this._http.get<FilterSection[]>(endpoint);
   }
 }
