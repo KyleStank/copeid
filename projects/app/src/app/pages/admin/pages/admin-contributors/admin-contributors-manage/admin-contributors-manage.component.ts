b4580e439da4b3db8102d322ae509c7600ad99d1
Kyle Stankovich <gamemakergml@gmail.com>
2021-09-25 17:48:02 -0400

    Added API pagination to contributors admin page

--- projects/app/src/app/pages/admin/pages/admin-contributors/admin-contributors-manage/admin-contributors-manage.component.ts
+++ projects/app/src/app/pages/admin/pages/admin-contributors/admin-contributors-manage/admin-contributors-manage.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
-import { BehaviorSubject, skipWhile, Subject, takeUntil } from 'rxjs';
+import { BehaviorSubject, from, map, Observable, skipWhile, Subject, takeUntil, tap, toArray } from 'rxjs';
 
 import { Contributor, ContributorService } from '@app/features';
+import { PaginationRequest } from '@core/models/pagination';
 import { ConfirmationAlertModalCompoonent } from '@shared/modals/confirmation-alert';
 import { AdminColumn } from '../../../common';
 import { IAdminManageView } from '../../../components';
@@ -27,6 +28,11 @@ export class AdminContributorsManageComponent implements IAdminManageView, OnIni
     { title: 'Description', property: 'description' }
   ];
   selectedItems: any[] = [];
+  paginatorLength = 0;
+  pageIndex = 0;
+  pageSize = 10;
+  cachedData: Contributor[][] = [];
+  get pageCount(): number { return Math.ceil(this.paginatorLength / this.pageSize); }
 
   constructor(
     private readonly _activatedRoute: ActivatedRoute,
@@ -41,10 +47,28 @@ export class AdminContributorsManageComponent implements IAdminManageView, OnIni
     this.getEntities();
   }
 
-  getEntities(): void {
-    this._contributorService.getAll({
+  getEntities(refreshCache: boolean = false): void {
+    this._getPagedEntities$(this.pageIndex, this.pageSize, refreshCache).subscribe({
+      next: results => this._contributorsSubject.next(results)
+    });
+  }
+
+  private _getPagedEntities$(pageIndex: number, pageSize: number, refreshCache: boolean): Observable<Contributor[]> {
+    const cachedItems = this.cachedData[pageIndex] ?? [];
+    if (!refreshCache && (this.cachedData.length === this.pageCount && cachedItems.length > 0)) {
+      return from(cachedItems).pipe(toArray());
+    }
+
+    return this._contributorService.getAllPaged(new PaginationRequest(pageIndex + 1, pageSize), {
       orderBy: ['name']
-    }).subscribe(this._contributorsSubject.next.bind(this._contributorsSubject));
+    }).pipe(
+      tap(response => {
+        this.paginatorLength = response?.count ?? this.paginatorLength;
+        this.cachedData = !refreshCache && this.cachedData.length === this.pageCount ? this.cachedData : new Array(this.pageCount).fill([]);
+        this.cachedData[pageIndex] = response?.data ?? [];
+      }),
+      map(response => response?.data ?? [])
+    );
   }
 
   editAddItem(model?: Contributor): void {
@@ -77,7 +101,7 @@ export class AdminContributorsManageComponent implements IAdminManageView, OnIni
           models!.forEach(m => {
             if (!!m?.id) {
               this._contributorService.delete(m.id).subscribe({
-                next: () => this.getEntities(),
+                next: () => this.getEntities(true),
                 error: (error: any) => console.error('Error:', error)
               });
             }
